63f1515fbf19b8f5075882f9d1cb2e473584e594
segraj27 <segrajalesci27@gmail.com>
2026-04-01 19:29:14 -0500

    ensayo 0001

--- src/app/api/mercadopago/route.ts
+++ src/app/api/mercadopago/route.ts
@@ -73,26 +73,25 @@ export async function POST(request: Request) {
         // pago con el pedido
         external_reference: pedido.id,
         back_urls: {
-          success: "http://localhost:3000/pago-exitoso",
-          failure: "http://localhost:3000/pago-error",
-          pending: "http://localhost:3000/pago-pendiente",
+          success:
+            "https://pasteleria-bizcocho-nextjs-supabase.vercel.app/checkout/success",
+          failure:
+            "https://pasteleria-bizcocho-nextjs-supabase.vercel.app//checkout/failure",
+          pending:
+            "https://pasteleria-bizcocho-nextjs-supabase.vercel.app//checkout/pending",
         },
 
         auto_return: "approved",
       },
     } as never);
 
-    return Response.json({ 
+    return Response.json({
       id: response.id,
       init_point: response.init_point,
     });
-
   } catch (error) {
     console.error("ERROR MP:", error);
 
-    return Response.json(
-      { error: "Error creando pago" },
-      { status: 500 }
-    );
+    return Response.json({ error: "Error creando pago" }, { status: 500 });
   }
 }
